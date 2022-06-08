bba97c0d0a5f1162c6d479afbaf5cb636da84210
DX9david <107140572+DX9david@users.noreply.github.com>
2022-06-08 19:43:59 +0200

    Update 268 fixer.bat

--- resources/268 fixer.bat
+++ resources/268 fixer.bat
@@ -2,17 +2,17 @@
 taskkill /IM RobloxPlayerLauncher.exe /F
 taskkill /IM RobloxPlayerBeta.exe /F
 taskkill /IM RobloxStudioLauncherBeta.exe /F
-del /Q C:\Users\[YOUR USERNAME HERE]\AppData\Local\Roblox\rbxcsettings.rbx
-del /Q C:\Users\[YOUR USERNAME HERE]\AppData\Local\Roblox\GlobalBasicSettings_13.xml
-del /Q C:\Users\[YOUR USERNAME HERE]\AppData\Local\Roblox\RobloxCookies.dat
-del /Q C:\Users\[YOUR USERNAME HERE]\AppData\Local\Roblox\frm.cfg
-del /Q C:\Users\[YOUR USERNAME HERE]\AppData\Local\Roblox\AnalysticsSettings.xml
-del /Q C:\Users\[YOUR USERNAME HERE]\AppData\Local\Roblox\LocalStorage\*
-del /S /Q C:\Users\[YOUR USERNAME HERE]\AppData\Local\Roblox\logs\*
+del /Q %localappdata%\rbxcsettings.rbx
+del /Q %localappdata%\GlobalBasicSettings_13.xml
+del /Q %localappdata%\Roblox\RobloxCookies.dat
+del /Q %localappdata%\Roblox\frm.cfg
+del /Q %localappdata%\Roblox\AnalysticsSettings.xml
+del /Q %localappdata%\Roblox\LocalStorage\*
+del /S /Q %localappdata%\Roblox\logs\*
 del /Q %temp%\RBX-*.log
 del /S /Q C:\Windows\Temp\*
-del /S /Q C:\Users\[YOUR USERNAME HERE]\AppData\Local\Microsoft\CLR_v4.0_32\UsageLogs\*
-del /S /Q C:\Users\[YOUR USERNAME HERE]\AppData\Local\Microsoft\CLR_v4.0\UsageLogs*
+del /S /Q %localappdata%\Microsoft\CLR_v4.0_32\UsageLogs\*
+del /S /Q %localappdata%\Microsoft\CLR_v4.0\UsageLogs*
 cls
 @ECHO Done! You can now play Roblox again.
 @ECHO You can close this now
